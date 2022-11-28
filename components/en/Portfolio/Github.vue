f6cb4a8d3f7487c49d158f2164c378beac4603cb
fares <djefaflia.fares@gmail.com>
2022-11-28 19:23:37 +0100

    add data for 'Projects' (#4)

--- components/en/Portfolio/Github.vue
+++ components/en/Portfolio/Github.vue
@@ -3,12 +3,12 @@
     <h1 class="font-bold text-color02 text-3xl">My github</h1>
     <p class="ml-2 font-light text-color02 text-sm mt-2">This is GitHub, you might find some clutter here 😒</p>
     <div class="mt-3 my-0 mx-auto sm:flex sm:flex-wrap sm:justify-between lg:max-w-2xl">
-      <a v-for="r in repos" :key="r" target="_blank" class="inline-block w-full py-2 rounded-sm transition-all lg:w-80 hover:bg-color02" :href="r.html_url" aria-label="link">
+      <a v-for="r in repos" :key="r" target="_blank" class="inline-block w-full py-2 px-2 rounded-sm transition-all lg:w-80 hover:bg-color02" :href="r.html_url" aria-label="link">
         <article class="flex">
-          <div class="flex justify-center items-center w-16">
+          <div class="flex justify-center items-center">
             <i class="text-lg text-color03 fi fi-brands-github h-5"></i>
           </div>
-          <div>
+          <div class="ml-1">
             <h2 class="text-color05 text-sm">{{"/" + r.full_name}}</h2>
           </div>
         </article>
--- components/en/Portfolio/Last.vue
+++ components/en/Portfolio/Last.vue
@@ -6,13 +6,13 @@
     <div class="mt-3 my-0 mx-auto sm:flex sm:flex-wrap sm:justify-between lg:max-w-2xl">
       <NuxtLink v-for="item in portfolio" :key="item" :to="item.link" class="inline-block w-full px-3 py-2 rounded-sm transition-all lg:w-80 hover:bg-color02" aria-label="link">
         <article class="flex">
-          <div class="flex justify-center items-center w-16">
+          <div class="flex justify-center items-center">
             <img class="w-12" :src="item.img">
           </div>
           <div class="ml-3">
             <h2 class="text-color05 font-medium">{{item.name}}</h2>
             <p class="text-color05 text-xs font-light">{{item.description}}</p>
-            <div class="flex items-center opacity-80">
+            <div class="flex items-center">
               <i class="text-xs text-color03 fi fi-brands-github h-3 mr-1"></i>
               <p class="text-xs text-color03">{{item.linkG}}</p>
             </div>
@@ -24,5 +24,5 @@
 </template>
 
 <script setup>
-  const {data: portfolio} = await useFetch('/portfolioAssets/api/projects.json')
+  const {data: portfolio} = await useFetch(' /portfolioAssets/api/last.json')
 </script>
