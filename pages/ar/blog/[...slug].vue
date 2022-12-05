2e208f7f7b7b5236d887707a05ae3ba918a24820
fares <djefaflia.fares@gmail.com>
2022-12-05 18:17:18 +0100

    add tocs links and fix style

--- pages/ar/blog/[...slug].vue
+++ pages/ar/blog/[...slug].vue
@@ -1,6 +1,22 @@
 <template>
-  <section class="py-20 px-10 sm:container sm:my-0 sm:mx-auto md:max-w-3xl 2xl:max-w-5xl text-white">
-    <ContentDoc class="article-blog" />
+  <section class="py-20 sm:py-0 px-10 sm:container sm:my-0 sm:mx-auto md:max-w-3xl 2xl:max-w-5xl text-white">
+    
+    <div class="big-title mb-10 sm:mb-0 flex justify-center items-center">
+      <h1 class="inline-block text-5xl sm:text-6xl text-color04 text-right w-72 rounded-md font-bold">{{data.title}}</h1>
+    </div>
+
+    <ul>
+      <h1 class="font-bold text-2xl text-color01 rounded-md inline-block p-1 bg-color04">الفهرسة</h1>
+      <li class="list-decimal mr-6 text-sm my-2" v-for="toc in data.body.toc.links" :key="toc" >
+        <NuxtLink :to="`#${toc.id}`">
+          {{ toc.text }}
+        </NuxtLink>
+      </li>
+    </ul>
+
+    <!-- the content  -->
+    <ContentDoc class="article-blog mt-8 text-justify" />
+    
     <div class="mt-5">
       <NuxtLink class="float-right inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:mr-1" to="/ar/blog/" aria-label="link">/رجوع</NuxtLink>
       <NuxtLink class="float-left inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:ml-1" to="/ar/portfolio/" aria-label="link">مشاركة</NuxtLink>
@@ -12,4 +28,20 @@
   definePageMeta({
     layout: 'arabic'
   })
+
+  //for add links - toc     <!-- {{data.body.toc.links}} -->
+  // assets: https://content.nuxtjs.org/guide/displaying/querying/
+const { path } = useRoute()
+
+const { data } = await useAsyncData(`content-${path}`, () => {
+  return queryContent().where({ _path: path }).findOne()
+})
 </script>
+
+<style scoped>
+@media (min-width:650px){
+  .big-title {
+      height: calc(100vh - 80px);
+    }
+}
+</style>
--- pages/ar/blog/index.vue
+++ pages/ar/blog/index.vue
@@ -27,4 +27,5 @@
   definePageMeta({
     layout: "arabic",
   })
+const { data } = await useAsyncData('home', () => queryContent('/ar/blog/').find())
 </script>
