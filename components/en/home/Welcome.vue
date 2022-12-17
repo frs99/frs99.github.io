d5911c5562df60cec173817efda93920ee63248d
fares <djefaflia.fares@gmail.com>
2022-12-17 17:49:50 +0100

    add to portfolio

--- components/en/home/Welcome.vue
+++ components/en/home/Welcome.vue
@@ -2,7 +2,7 @@
   <div class="text-color05 px-6 sm:relative sm:bottom-10">
     <p class="text-right font-light">– Austin Freeman</p>
     <h1 class="font-black text-3xl lg:text-6xl 2xl:text-7xl mt-3">Simplicity Is The Soul</h1>
-    <h1 class="font-black text-3xl lg:text-6xl 2xl:text-7xl lg:text-right mt-3">Of Efficiency</h1>
-    <NuxtLink class="inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:ml-1" to="/about/" aria-label="link">/About</NuxtLink>
+    <h1 class="font-black text-3xl lg:text-6xl 2xl:text-7xl lg:text-right lg:mt-3">Of Efficiency</h1>
+    <NuxtLink class="inline-block float-right sm:float-left bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:ml-1" to="/about/" aria-label="link">/About</NuxtLink>
   </div>
 </template>
