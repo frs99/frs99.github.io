eb6b305f0a2841fcba50bd1de043304e4ca1709e
fares <djefaflia.fares@gmail.com>
2022-12-20 19:22:54 +0100

    fix blog and content

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="py-20 px-10 sm:container sm:my-0 sm:mx-auto md:max-w-3xl">
     <h1 class="font-bold text-color03 text-3xl">المدونة</h1>
-    <p class="font-light text-color03 text-sm mt-2">في هذه المساحة اضع ما اجربه او ما اتعلمه واريد مشاركته او الاحتفاظ به.</p>
+    <p class="font-light text-color03 text-sm mt-2">في هذه المساحة اشارك خططي وافكاري و ما تعلمته في عالم التقنية.</p>
     <p class="font-light text-color05 bg-color02 p-3 rounded-lg text-sm mt-2">لا يتم اضافة اعلانات او روابط مشبوهة مختصرة هنا.</p>
 
     <ContentList path="/blog" v-slot="{ list }">
