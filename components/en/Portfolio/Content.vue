a05fa81054c83ed269379b26ef9309cc3a864fc3
fares <djefaflia.fares@gmail.com>
2023-02-20 18:59:01 +0100

    update

--- components/en/Portfolio/Content.vue
+++ components/en/Portfolio/Content.vue
@@ -2,7 +2,7 @@
   <div class="px-6">
     <EnPortfolioContentFuture />
     <EnPortfolioContentProjects />
-    <EnPortfolioContentLab />
+    <!-- <EnPortfolioContentLab /> -->
     <EnPortfolioContentGithub />
     <div class="mt-5">
       <NuxtLink class="float-left inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:ml-1" to="/en/about" aria-label="link">cd..</NuxtLink>
