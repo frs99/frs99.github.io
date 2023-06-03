4459689e3e60c4a09fa2156f8810c30306295dbf
fares <djefaflia.fares@gmail.com>
2023-06-03 18:31:27 +0100

    v5.1.3 - update page about

--- components/ar/About/Summary.vue
+++ components/ar/About/Summary.vue
@@ -1,44 +1,83 @@
 <template>
+  <!-- START ABOUT TEXT -->
   <div class="text-color05 w-4/5 sm:w-3/5">
-    
-    <h1>فارس<br>جفافلية</h1>
-    <br>
 
-    <p>السلام عليكم، اسمي <strong>فارس</strong> وعمري 23 و احب المنطق و التحليل،
-    <strong>تقني في شبكات الاتصال</strong>، <strong>مطور مواقع الويب</strong>، ومهتم بتصميم UX/UI واحب التعلم و طرح مشاريع مفتوحة المصدر.</p>
-    <p>كذلك مهتم جدا بعلوم الشريعة وأقسامها.</p>
+  <h1>فارس<br>جفافلية</h1>
 
-    <br>
-    
-    <p>عملت سنتين لدى &quot;<strong><a  target="_blank" href="https://www.algerietelecom.dz/ar">اتصالات الجزائر</a></strong>&quot;، و أول موقع إلكتروني ابرمجه كان منذ حوالي 3 سنوات.</p>
-    <br>
+  <br>
 
-    <p>اجيد استخدام الكمبيوتر و الانترنت في عدة اشياء، أغلبها في البرمجة و التصميم، واجيد لغات البرمجة الأساسية لاجل مطور ويب الواجهة الامامية، واجيد استخدام إطار العمل <strong><a  target="_blank" href="https://vuejs.org/">Vue.js@</a></strong> و <strong><a  target="_blank" href="https://nuxt.com/">Nuxt.js@</a></strong> و <strong><a  target="_blank" href="https://tailwindcss.com/">TailwindCSS@</a></strong></p>
-    <br>
 
-    <p>حاليا أعمل على عدة مشاريع ومن بينها <strong><a  target="_blank" href="https://github.com/esaturn">ESaturn@</a></strong> و هو مختبر أنشئ فيه أدوات تدعم وتساعد مطوري الويب، و اعمل على <strong><a  target="_blank" href="https://github.com/gheyth">@غيث</a></strong> و محتواه لخدمة الإسلام، ومشاريع اخرى عديدة تجدها في صفحة <strong><NuxtLink to="/portfolio/">أعمالي</NuxtLink></strong>.</p>
-    <br>
 
-    <p>مستقبلا اخطط لتعلم تطوير الواجهة الخلفية للمواقع والدخول الى عالم <strong><a  target="_blank" href="https://flutter.dev/">flutter</a></strong></p>
-    <br>
+  <p>السلام عليكم، اسمي <strong>فارس</strong> عمري {{age}} و احب المنطق و التحليل،
 
-    <p>من اجل سيرتي الذاتية أو معرفة المزيد عني يمكنك إرسال <strong><a  target="_blank" href="mailto:frs99.me@gmail.com">بريد إلكتروني</a></strong>.
-    <br>
+  <strong>تقني في شبكات الاتصال</strong>، <strong>مطور مواقع الويب</strong>، ومهتم بتصميم UX/UI واحب التعلم و طرح المشاريع مفتوحة المصدر.</p>
 
-    يمكنك أن تجدني في <strong><a  target="_blank" href="https://twitter.com/_frs99">Twitter</a></strong> او <strong><a  target="_blank" href="https://github.com/frs99">Github</a></strong> او <strong><a  target="_blank" href="https://www.facebook.com/frsdje">Facebook</a></strong> .</p>
-    <br>
+  <p>كذلك مهتم جدا بعلوم الشريعة وأقسامها.</p>
 
-    <p class="font-light text-sm text-left"><a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a> FARES {{year}}</p>
-    <div class="mt-5">
-      <NuxtLink class="float-right inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:mr-1" to="/" aria-label="link">/رجوع</NuxtLink>
-      <NuxtLink class="float-left inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:ml-1" to="/portfolio/" aria-label="link">/أعمالي</NuxtLink>
-    </div>
+
+
+  <br>
+
+  <p>عملت سنتين لدى &quot;<strong><a target="_blank" href="https://www.algerietelecom.dz/ar">اتصالات الجزائر</a></strong>&quot;، و أول موقع إلكتروني ابرمجه كان في 2019 .</p>
+
+  <br>
+
+
+
+  <p>اجيد استخدام الكمبيوتر و الانترنت في عدة اشياء، أغلبها في البرمجة و التصميم، واجيد لغات البرمجة الأساسية لاجل مطور ويب الواجهة الامامية، واجيد استخدام إطار العمل <strong><a target="_blank" href="https://vuejs.org/">Vue.js@</a></strong> و <strong><a target="_blank" href="https://nuxt.com/">Nuxt.js@</a></strong> و <strong><a target="_blank" href="https://tailwindcss.com/">TailwindCSS@</a></strong></p>
+
+  <br>
+
+
+
+  <p>ايضا اجيد استخدام عدة برامج من ادوبي مثل: AdobeXD , Illustrator</p>
+
+  <br>
+
+
+
+  <p>حاليا أعمل على عدة مشاريع ومن بينها <strong><a target="_blank" href="https://github.com/himzml">Muzaml@</a></strong> وهو مشروع خاص يرعى ويساعد أصحاب الأنشطة الرقمية، و اعمل على <strong><a target="_blank" href="https://github.com/gheyth">@غيث</a></strong> و محتواه لخدمة الإسلام، ومشاريع اخرى عديدة تجدها في صفحة <strong><NuxtLink to="/portfolio/">أعمالي</NuxtLink></strong>.</p>
+
+  <br>
+
+
+
+  <p>مستقبلا اخطط لتعلم تطوير الواجهة الخلفية للمواقع والدخول الى عالم <strong><a target="_blank" href="https://flutter.dev/">flutter</a></strong></p>
+
+  <br>
+
+
+
+  <p>من اجل سيرتي الذاتية أو معرفة المزيد عني يمكنك إرسال <strong><a target="_blank" href="mailto:frs99.me@gmail.com">بريد إلكتروني</a></strong>.
+
+  <br>
+
+
+
+  يمكنك أن تجدني في <strong><a target="_blank" href="https://twitter.com/_frs99">Twitter</a></strong> او <strong><a target="_blank" href="https://github.com/frs99">Github</a></strong> او <strong><a target="_blank" href="https://t.me/faresdjefaflia">Telegram</a></strong> .</p>
+
+  <br>
+
+
+
+  <p class="font-light text-sm text-left"><a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a> FARES {{ year }}</p>
+
+  <div class="mt-5">
+
+  <NuxtLink class="float-right inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:mr-1" to="/" aria-label="link">/رجوع</NuxtLink>
+
+  <NuxtLink class="float-left inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:ml-1" to="/portfolio/" aria-label="link">/أعمالي</NuxtLink>
+
   </div>
+
+  </div>
+  <!-- END ABOUT TEXT -->
 </template>
 
 <script setup>
   let d = new Date
   let year = d.getFullYear()
+  let age = year - 1999
 </script>
 
 <style scoped>
--- components/en/About/Summary.vue
+++ components/en/About/Summary.vue
@@ -1,40 +1,65 @@
 <template>
   <div class="text-color05 w-4/5 sm:w-3/5">
-    <h1>Fares <br> Djefaflia</h1>
+
+    <h1>Fares<br>Djefaflia</h1>
+
     <br>
 
-    <p>Hey, my name is <strong>Fares</strong> and im {{year - 1999}} year, I love logic and analysis.
-    I&#39;m also a <strong>network technician</strong> & <strong>website developer</strong>, and interested in UX/UI design, and I love open source projects.</p>
+    <p>Hello, my name is <strong>Fares</strong>. I am {{ age }} years old and I love logic and analysis.</p>
+
+    <p>I am a <strong>network technician</strong> and a <strong>web developer</strong>, and I am interested in UX/UI design. I enjoy learning and contributing to open-source projects.</p>
+
+    <p>I am also very interested in the sciences of Sharia and its branches.</p>
+
     <br>
 
-    <p>I worked for two years at Algeria Telecom, and the first website I programmed was about {{year - 2019}} years ago.</p>
+    <p>I worked for two years at "<strong><a target="_blank" href="https://www.algerietelecom.dz/ar">Algerie Telecom</a></strong>," and the first website I programmed was in 2019.</p>
+
     <br>
 
-    <p>I am good at using a computer and the Internet in several things, mostly in programming and design, and I am good at basic programming languages for a front-end web developer, and I am good at using the <strong><a href="https://vuejs.org/" target="_blank" title="@Vue.js">@Vue.js</a></strong> and <strong><a href="https://nuxt.com/" target="_blank" title="@Nuxt.js">@Nuxt.js</a></strong> and <strong><a href="https://tailwindcss.com/" target="_blank" title="Tailwind CSS">Tailwind CSS</a></strong> frameworks.</p>
+    <p>I am proficient in using computers and the internet for various purposes, mostly in programming and design. I am skilled in core programming languages for front-end web development, and I am proficient in using the <strong><a target="_blank" href="https://vuejs.org/">Vue.js</a></strong>, <strong><a target="_blank" href="https://nuxt.com/">Nuxt.js</a></strong>, and <strong><a target="_blank" href="https://tailwindcss.com/">TailwindCSS</a></strong> frameworks.</p>
+
     <br>
 
-    <p>I am currently working on several projects, including the <strong><a href="https://github.com/esaturn" target="_blank" title="@ESaturn">@ESaturn</a></strong>, which supports and assists web developers, and I am working on the &quot; Hadinat <strong><a href="https://github.com/gheyth" target="_blank" title="@Gheyth">@Gheyth</a></strong> &quot; platform and its religious-spiritual content, and many other projects, that you can find in <strong><NuxtLink to="/portfolio/" title="my portfolio">my portfolio</NuxtLink></strong>.</p>
+    <p>I also have proficiency in using various Adobe programs such as Adobe XD and Illustrator.</p>
+
     <br>
 
-    <p>In the future, I plan to learn the back-end development of websites and enter the world of <strong><a href="https://flutter.dev/" target="_blank" title="Flutter">Flutter</a></strong></p>
+    <p>Currently, I am working on several projects, including <strong><a target="_blank" href="https://github.com/himzml">Muzaml</a></strong>, a special project that supports and assists digital activities. I am also working on <strong><a target="_blank" href="https://github.com/gheyth">@Gheyth</a></strong>, which provides content for the service of Islam, along with other numerous projects that you can find on my <strong><a target="_blank" href="/portfolio/">portfolio page</a></strong>.</p>
+
     <br>
 
-    <p>For my resume or to know more about me, you can <strong><a target="_blank" href="mailto:frs99.me@gmail.com" title="contact me">Email me</a></strong></p>
-    <p>Find me on <strong><a href="https://twitter.com/_frs99" target="_blank" title="Twitter">Twitter</a></strong>, <strong><a href="https://github.com/frs99" target="_blank" title="Github">Github</a></strong> or <strong><a href="https://www.facebook.com/me.frs99" target="_blank" title="Facebook">Facebook</a></strong>.</p>
-    
+    <p>In the future, I plan to learn back-end web development and delve into the world of <strong><a target="_blank" href="https://flutter.dev/">Flutter</a></strong>.</p>
+
     <br>
-    <hr class="w-5 mb-5">
-    <p class="font-light text-sm"><a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a> FARES {{year}}</p>
+
+    <p>If you would like to view my resume or learn more about me, you can send an <strong><a target="_blank" href="mailto:frs99.me@gmail.com">email</a></strong>.</p>
+
+    <br>
+
+    <p>You can find me on <strong><a target="_blank" href="https://twitter.com/_frs99">Twitter</a></strong>, <strong><a target="_blank" href="https://github.com/frs99">GitHub</a></strong>, or <strong><a target="_blank" href="https://t.me/faresdjefaflia">Telegram</a></strong>.</p>
+
+    <br>
+
+    <p class="font-light text-sm text-left"><a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a> FARES {{ year }}</p>
+
     <div class="mt-5">
-      <NuxtLink class="float-left inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:ml-1" to="/en/" aria-label="link">cd..</NuxtLink>
-      <NuxtLink class="float-right inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:mr-1" to="/en/portfolio/" aria-label="link">/Portfolio</NuxtLink>
+
+    <a class="float-right inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:mr-1" href="/" aria-label="link">
+
+  /Back</a>
+
+    <a class="float-left inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:ml-1" href="/portfolio/" aria-label="link">/My Works</a>
+
     </div>
+
   </div>
 </template>
 
 <script setup>
   let d = new Date
   let year = d.getFullYear()
+  let age = year - 1999
 </script>
 <style scoped>
   h1{
