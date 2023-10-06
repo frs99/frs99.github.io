ecc2a888660491f0593d72830ef008cf3dca80df
fares <djefaflia.fares@gmail.com>
2023-10-06 17:38:44 +0100

    test @tailwindcss/typography #9

--- pages/blog/tech/[...slug].vue
+++ pages/blog/tech/[...slug].vue
@@ -15,7 +15,7 @@
     </ul>
 
     <!-- the content  -->
-    <ContentDoc class="article-blog mt-8 text-justify" />
+    <ContentDoc class="article-blog mt-8 text-justify prose-them01 prose-lg" />
     
     <div class="mt-5">
       <NuxtLink class="float-right inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:mr-1" to="/blog/tech/" aria-label="link">/رجوع</NuxtLink>
