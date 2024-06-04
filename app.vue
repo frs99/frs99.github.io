92493d9a62b227ef8edf9a9c0c5a522eec16b0dc
fares <djefaflia.fares@gmail.com>
2024-06-04 13:41:35 +0100

    Abandon external css files #12

--- app.vue
+++ app.vue
@@ -17,4 +17,53 @@
   opacity: 0;
   filter: blur(1rem);
 }
+
+
+* {
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
+  list-style: none;
+  text-decoration: none;
+  -webkit-tap-highlight-color: transparent;
+  /* text-align: justify; */
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+input:focus {
+  outline: none;
+}
+
+/* SCROLLBAR */
+::-webkit-scrollbar {
+  height:8px;
+  width: 6px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background-color: #C9ADA7;
+  border-radius: 2px;
+}
+
+/* For mozila */
+/* @-moz-document url-prefix() {} */
+
+/**********************/
+
+a {
+  user-select: none;
+}
+img{
+  pointer-events: none;
+}
+.max{
+  max-width: 1920px;
+}
+body{
+  background-color: #22223B;
+}
 </style>
