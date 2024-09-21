0ee83718ab8c274757d2f6625c5d923cda2bfc8e
fares <djefaflia.fares@gmail.com>
2024-09-21 13:14:39 +0100

    fix all links

--- pages/blog/islam/index.vue
+++ pages/blog/islam/index.vue
@@ -29,6 +29,10 @@
         </li>
       </ul>
     </ContentList>
+    <div class="mt-5">
+      <NuxtLink class="float-right inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:mr-1" to="/portfolio/" aria-label="link">/رجوع</NuxtLink>
+      <NuxtLink class="float-left inline-block bg-color04 text-color01 font-medium px-6 py-2 rounded-md mt-3 transition-all hover:ml-1" to="/uses/" aria-label="link">/إستخداماتي</NuxtLink>
+    </div>
   </section>
 </template>
 
